6472a225e758297f8955500f8337903fe492b1c8
joey@emakefun.com <joey@emakefun.com>
2021-05-20 12:49:35 +0800

    add motors

--- MarRobot.ts
+++ MarRobot.ts
@@ -15,6 +15,17 @@ namespace MarRobot {
 		 S7 = 0x06,
          S8 = 0x07
     }
+	
+	export enum Motors {
+         S1 = 0x00,
+         S2 = 0x01,
+		 S3 = 0x02,
+         S4 = 0x03,
+		 S5 = 0x04,
+         S6 = 0x05,
+		 S7 = 0x06,
+         S8 = 0x07
+    }
 
     let servo1Degree = 0;
     let servo2Degree = 0;
@@ -29,9 +40,10 @@ namespace MarRobot {
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     //% subcategory="执行器_Actuator"
     export function Servo(index: Servos, degree: number): void {
-        let buf = pins.createBuffer(2);
-        buf[0] = index;
-        buf[1] = Math.round(Math.map(degree, 0, 180, 50, 250));
+        let buf = pins.createBuffer(3);
+		buf[0] = 0x00;
+        buf[1] = index;
+        buf[2] = Math.round(Math.map(degree, 0, 180, 50, 250));
         pins.i2cWriteBuffer(MarRobot_IICADDRESS, buf);
         
         // if (!initialized) {
@@ -86,4 +98,77 @@ namespace MarRobot {
             }
         }
     }
+	
+	//% blockId=roverbit_motor_run block="Motor|%index|speed %speed"  group="Motor 电机"
+    //% weight=85
+    //% speed.min=-100 speed.max=100
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% subcategory="执行器_Actuator"
+    export function SportMotorRun(index: Motors, speed: number): void {
+        MotorRun(index, number);
+    }
+	
+	function MotorRun(index: number, speed: number): void {
+		let buf = pins.createBuffer(3);
+		buf[0] = 0x01;
+        buf[1] = index;
+        buf[2] = speed;
+        pins.i2cWriteBuffer(MarRobot_IICADDRESS, buf);
+    }
+	
+	/**
+     * Execute two motors at the same time
+     * @param motor1 First Motor; eg: M1, M2
+     * @param speed1 [-255-255] speed of motor; eg: 150, -150
+     * @param motor2 Second Motor; eg: M3, M4
+     * @param speed2 [-255-255] speed of motor; eg: 150, -150
+    */
+    //% blockId=roverbit_motor_dual block="Motor|%motor1|speed %speed1|%motor2|speed %speed2"  group="Motor 电机"
+    //% weight=84
+    //% speed1.min=-255 speed1.max=255
+    //% speed2.min=-255 speed2.max=255
+    //% inlineInputMode=inline
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% subcategory="执行器_Actuator"
+    export function MotorRunDual(motor1: Motors, speed1: number, motor2: Motors, speed2: number): void {
+        MotorRun(motor1, speed1);
+        MotorRun(motor2, speed2);
+    }
+	
+	/**
+     * Execute single motors with delay
+     * @param index Motor Index; eg: M1, M2, M3, M4
+     * @param speed [-255-255] speed of motor; eg: 150, -150
+     * @param delay seconde delay to stop; eg: 1
+    */
+    //% blockId=roverbit_motor_rundelay block="Motor|%index|speed %speed|delay %delay|s"  group="Motor 电机"
+    //% weight=81
+    //% speed.min=-255 speed.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% subcategory="执行器_Actuator"
+    export function SportMotorRunDelay(index: Motors, speed: number, delay: number): void {
+        MotorRunDelay(index, speed, delay);
+    }
+
+    function MotorRunDelay(index: number, speed: number, delay: number): void {
+        MotorRun(index, speed);
+        basic.pause(delay * 1000);
+        MotorRun(index, 0);
+    }
+    
+	/**
+     * Execute single motors with delay
+     * @param index Motor Index; eg: M1
+    */
+    //% blockId=roverbit_stop block="Sport Motor Stop|%index|" group="Motor 电机"
+    //% weight=80
+    //% subcategory="执行器_Actuator"
+    export function SportMotorStop(index: Motors): void {
+        MotorStop(index);
+    }
+
+    export function MotorStop(index: number): void {
+        MotorRun(index, 0);
+    }
+
 }
